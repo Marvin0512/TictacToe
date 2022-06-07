51538b9f6cffab362ebb505f168e9a3e5f076fc9
marvin.b7 <marvin.b7@P-0G-0B113-0446>
2022-06-07 14:41:23 +0200

    Letzte_Änderung_Schule_07.06

--- WindowsFormsApp1/WindowsFormsApp1/CustomControl1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/CustomControl1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class CustomControl1
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -20,28 +20,17 @@
             base.Dispose(disposing);
         }
 
-        #region Vom Windows Form-Designer generierter Code
+        #region Vom Komponenten-Designer generierter Code
 
         /// <summary>
-        /// Erforderliche Methode für die Designerunterstützung.
+        /// Erforderliche Methode für die Designerunterstützung. 
         /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1122, 616);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            components = new System.ComponentModel.Container();
         }
 
         #endregion
     }
 }
-
---
+++ WindowsFormsApp1/WindowsFormsApp1/CustomControl1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class CustomControl1 : Control
+    {
+        public CustomControl1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+
+            RectangleF[] rects =
+            {
+                new Regta
+            }
+
+
+            //Rectangle frameRectangle = new Rectangle(0, 0, 30, 30);
+            //g.DrawRectangles(Pens.Black, frameRectangle[],rects);
+
+        }
+    }
+}
+//https://docs.microsoft.com/de-de/dotnet/api/system.drawing.graphics.drawrectangles?view=dotnet-plat-ext-6.0#system-drawing-graphics-drawrectangles(system-drawing-pen-system-drawing-rectanglef())
